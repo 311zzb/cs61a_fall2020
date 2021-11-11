e157ce6fe598faa76fab04e66dbe09a40a405285
311zzb <48852285+311zzb@users.noreply.github.com>
2021-11-11 22:46:33 +0800

    Add files via upload

---
+++ lab/lab01/lab01.py
@@ -0,0 +1,65 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    result = 1
+    while k > 0:
+        result *= n
+        k -= 1
+        n -= 1
+    return result
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    sum = 0
+    while y > 0:
+        last_digit = y % 10
+        sum += last_digit
+        y = (y - last_digit) // 10
+    return sum
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    while n > 0:
+        if n % 10 == 8:
+            n = n // 10
+            if n % 10 == 8:
+                return True
+        else:
+            n = n // 10
+    return False
